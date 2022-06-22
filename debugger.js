9414e5ddf177d652961ab969a9c7a8e43b38e3d3
dezhizhang <1541609448@qq.com>
2022-06-23 04:54:10 +0800

    feat:loader

---
+++ debugger.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const config = require('./webpack.config');
+
+
+debugger;
+const compiler = webpack(config);
+compiler.run((err,stats) => {
+    console.log(stats.toJson({
+        assets:true,
+        chunks:true,
+        modules:true
+    }))
+})
